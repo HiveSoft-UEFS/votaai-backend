fa05f529f6580f0cdaf999937738a354f5c359ad
Cláudia <claudiainees@hotmail.com>
2024-06-27 21:08:05 -0300

    teste

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import UserSerializer
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- votaai_backend/settings.py
+++ votaai_backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'votaai_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'votaai',     
+        'USER': 'votaai',   
+        'PASSWORD': 'votaai', 
+        'HOST': 'localhost',          
+        'PORT': '5432',               
     }
 }
 
@@ -122,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app.User'
