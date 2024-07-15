4c78771ba7a04293e40882de4940cbb5ef5ac0bb
SivalLeao <leaosival@gmail.com>
2024-07-15 11:44:43 -0300

    adding function to return voting quantity values

--- app/views/user_view.py
+++ app/views/user_view.py
@@ -1,10 +1,16 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import viewsets
-from app.serializers.user_serializer import UserSerializer
+from app.serializers.user_serializer import UserSerializer, ChangePasswordSerializer
 from app.services.user_service import UserService
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
+
+
+#from app.serializers.ChangePasswordSerializer import ChangePasswordSerializer
+
+
 
 class UserViewSet(viewsets.ViewSet):
     _service = UserService()
@@ -94,21 +100,39 @@ class UserViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['get', 'patch'], url_path='profile', permission_classes=[IsAuthenticated])
     def user_profile(self, request):
-        user_id = request.user.id
+        user_id = request.user.id  # Usa o ID do usuário autenticado
         user = self._service.get_user_by_id(user_id)
 
         if request.method == 'GET':
             if user['success']:
                 return Response(user['data'], status=status.HTTP_200_OK)
             return Response({'error': user['error']}, status=status.HTTP_404_NOT_FOUND)
-        
+
         if request.method == 'PATCH':
             if user['success']:
-                serializer = UserSerializer(user['data'], data=request.data, partial=True)
+                user_instance = user['data']  # Certifique-se de que isso seja uma instância do User
+                
+                if not isinstance(user_instance, User):
+                    return Response({'error': 'Usuário não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+                # Atualizar senha se os dados forem fornecidos
+                if 'current_password' in request.data and 'new_password' in request.data:
+                    current_password = request.data['current_password']
+                    new_password = request.data['new_password']
+
+                    if user_instance.check_password(current_password):
+                        user_instance.set_password(new_password)
+                        user_instance.save()
+                        return Response({'message': 'Senha atualizada com sucesso.'}, status=status.HTTP_200_OK)
+                    else:
+                        return Response({'error': 'Senha atual incorreta.'}, status=status.HTTP_400_BAD_REQUEST)
+
+                # Atualizar email e username
+                serializer = UserSerializer(user_instance, data=request.data, partial=True)
                 if serializer.is_valid():
-                    updated_user = self._service.update_user(user['data'], serializer.validated_data)
+                    updated_user = self._service.update_user(user_instance, serializer.validated_data)
                     if updated_user['success']:
                         return Response(updated_user['data'], status=status.HTTP_200_OK)
                     return Response({'error': updated_user['error']}, status=status.HTTP_400_BAD_REQUEST)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
             return Response({'error': user['error']}, status=status.HTTP_404_NOT_FOUND)
