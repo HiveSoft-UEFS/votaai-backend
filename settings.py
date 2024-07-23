5eda1a589c1ba1430800bde14169fc1ce22d407e
micaelmz <micaelmuniz6@gmail.com>
2024-07-23 01:34:37 -0300

    postgres nos requisitos

--- settings.py
+++ settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +80,7 @@ WSGI_APPLICATION = 'wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'votaai',     
-        'USER': 'votaai',   
-        'PASSWORD': 'votaai', 
-        'HOST': 'localhost',          
-        'PORT': '5432',               
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
